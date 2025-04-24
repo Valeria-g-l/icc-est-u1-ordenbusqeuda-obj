ce1f67eb61c48cf54be1f714ab44dbb743b09ded
Valeria <kguamanleon03@est.ups.edu.ec>
2025-04-24 08:54:29 -0500

    Busqueda Objeto

---
+++ src/models/Direccion.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Direccion {
+    int calle;
+    int numeroCalle;
+    int codigo;
+
+    public Direccion(String string, int i, int j) {
+        
+    }
+    public int getCalle() {
+        return calle;
+    }
+    public void setCalle(int calle) {
+        this.calle = calle;
+    }
+    public int getNumeroCalle() {
+        return numeroCalle;
+    }
+    public void setNumeroCalle(int numeroCalle) {
+        this.numeroCalle = numeroCalle;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    
+}
---
+++ src/views/ViewConsole.java
@@ -0,0 +1,12 @@
+package views;
+import models.Persona;
+
+public class ViewConsole {
+
+    public void printPersonas(Persona[]personas){
+        
+
+        
+    }
+    
+}
